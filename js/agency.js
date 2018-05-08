fa1b68ba5d2928bc2a117ff88c2685bcc88224d8
Ronald Chen <ronfung95@gmail.com>
2018-05-08 17:31:14 -0400

    update
    fixed sales rep
    need to work on products / collapsing navbar

--- js/agency.js
+++ js/agency.js
@@ -33,12 +33,12 @@
     } else {
       $("#mainNav").removeClass("navbar-shrink");
     }
-  };
+  }; 
   // Collapse now if page is not at top
   navbarCollapse();
   // Collapse the navbar when page is scrolled
   $(window).scroll(navbarCollapse);
-  */
+ */
 
   // Hide navbar when modals trigger
   $('.portfolio-modal').on('show.bs.modal', function(e) {
@@ -47,5 +47,9 @@
   $('.portfolio-modal').on('hidden.bs.modal', function(e) {
     $(".navbar").removeClass("d-none");
   })
+  
+$('.dropdown-item').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
 
 })(jQuery); // End of use strict
